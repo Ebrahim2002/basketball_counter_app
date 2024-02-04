2fa78363120e44378c7daa6895652db003aba71e
Ebrahim <hemaaibrahem8@gmail.com>
2024-02-04 23:56:02 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'cubit/counter_cubit.dart';
-
+//
 void main() {
   runApp(const PointsCounter());
 }
